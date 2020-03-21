e479c663e0b15c744fa0be84df8e2e6d0e24d20d
jmajurel <jeanfelix.majurel@gmail.com>
2020-03-22 00:09:57 +0100

    commit missing update

--- src/routers/conditions.ts
+++ src/routers/conditions.ts
@@ -1,12 +1,15 @@
 import { route, GET, POST, before } from "awilix-express";
 import IConditionSInterface from "../services/interfaces/conditionSInterface";
 import { Request, Response } from "express";
-import ConditionService from "..//services/conditionService";
 
 @route("/conditions")
 export default class ConditionAPI {
   private readonly _conditionService: IConditionSInterface;
-  constructor({ conditionService }: { conditionService: ConditionService }) {
+  constructor({
+    conditionService
+  }: {
+    conditionService: IConditionSInterface;
+  }) {
     this._conditionService = conditionService;
   }
   @route("/:id")
